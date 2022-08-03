22c6188eeaa0608147ea9280c14d2c3eb5397cca
munenebrian <njerubrianmunene@gmail.com>
2022-08-04 00:39:12 +0300

    finishing up components!

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
         </div>
 
         <div>
-          <img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
+          <img src={hero1.jpeg} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
         </div>
       </div>
     </div>
