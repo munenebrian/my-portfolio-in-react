1a8f39316408242ac64ba29c6847d15bcc3a9e3f
munenebrian <njerubrianmunene@gmail.com>
2022-08-03 19:01:16 +0300

    finishing on the components!

--- my-portfolio/src/components/Experience.js
+++ my-portfolio/src/components/Experience.js
@@ -63,8 +63,7 @@ const Experience = () => {
 
   return (
     <div
-      name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
@@ -77,8 +76,7 @@ const Experience = () => {
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
-              key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+              key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
               <img src={src} alt="" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
