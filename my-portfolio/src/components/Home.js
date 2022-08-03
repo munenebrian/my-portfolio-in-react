31876255404dea104b80861e3dda865344ee0621
munenebrian <njerubrianmunene@gmail.com>
2022-08-04 00:28:27 +0300

    finishing up components!

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -9,12 +9,11 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm an aspiring Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            I am currently an undergrad student. Brian has passion for web development and I love to work on web application using technologies like
+            React, Tailwind, JavaScript and HTML,CSS.
           </p>
 
           <div>
